9c4299aaf369811750984ea949a7b75025bfb3ae
Frida Estrada <fridaestrada24@gmail.com>
2024-08-27 11:04:07 -0600

    Switch calculadora cientifica

--- calculadora/src/com/calculadora/CalculadoraCientifica.java
+++ calculadora/src/com/calculadora/CalculadoraCientifica.java
@@ -1,5 +1,8 @@
 package com.calculadora;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class CalculadoraCientifica extends AbstractCalculadora {
 
     //potencia,raiz, seno,cos,tang
@@ -46,6 +49,85 @@ public class CalculadoraCientifica extends AbstractCalculadora {
 
     @Override
     protected double switchCalculadora(int opcion) {
-        return 0;
+        double resultado= 0;
+        boolean running = true;
+        boolean running2 = true;
+        CalculadoraBasica calc= new CalculadoraBasica();
+        if (opcion > 5) running = false;
+                else running2 = false;
+        while(running) {
+            try {
+                System.out.println("ingresa dos numeros");
+                double x = new Scanner(System.in).nextDouble();
+                double y = new Scanner(System.in).nextDouble();
+                switch (opcion) {
+                    case 1: {
+                        resultado = calc.sumar(x, y);
+                        running = false;
+                        break;
+                    }
+                    case 2: {
+                        resultado = calc.restar(x, y);
+                        running = false;
+                        break;
+                    }
+                    case 3: {
+                        resultado = calc.multiplicar(x, y);
+                        running = false;
+                        break;
+                    }
+                    case 4: {
+                        resultado = calc.dividir(x, y);
+                        running = false;
+                        break;
+                    }
+                    case 5: {
+                        resultado = this.potencia(x, y);
+                        running = false;
+                        break;
+                    }
+                    default: {
+                        System.out.println("opcion no valida");
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Ingresa numeros validos");
+            }
+        }
+
+        while(running2) {
+            try {
+                System.out.println("ingresa un numero");
+                double x = new Scanner(System.in).nextDouble();
+                switch (opcion) {
+                    case 6: {
+                        resultado = this.raizCuadrada(x);
+                        running2 = false;
+                        break;
+                    }
+                    case 7: {
+                        resultado = this.seno(x);
+                        running2 = false;
+                        break;
+                    }
+                    case 8: {
+                        resultado = this.coseno(x);
+                        running2 = false;
+                        break;
+                    }
+                    case 9: {
+                        resultado = this.tangente(x);
+                        running2 = false;
+                        break;
+                    }
+                    default: {
+                        System.out.println("opcion no valida");
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Ingresa numeros validos");
+            }
+        }
+        return resultado;
     }
 }
--- calculadora/src/com/calculadora/Main.java
+++ calculadora/src/com/calculadora/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        AbstractCalculadora calculadora = new CalculadoraBasica();
+        AbstractCalculadora calculadora = new CalculadoraCientifica();
         try {
             System.out.println(calculadora.imprimirMenu());
             int opcion = scanner.nextInt();
