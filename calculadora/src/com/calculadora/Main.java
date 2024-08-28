6a46a3f55619899947b3544b0ccb20b4c0a3a2fe
Andres Pardo <jorgeandres.pardorea@gmail.com>
2024-08-28 10:40:24 -0600

    menu eleccion de calculadora

--- calculadora/src/com/calculadora/Main.java
+++ calculadora/src/com/calculadora/Main.java
@@ -4,9 +4,21 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        AbstractCalculadora calculadora = new CalculadoraCientifica();
+        AbstractCalculadora calculadora = null;
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Bienvenido");
+        System.out.println("¿Qué calculadora te gustaría usar");
+        System.out.print("Opciones \n 1.Calculadora Básica \n 2.Calculadora cientifica\n");
+        String calculadoraSeleccion = sc.next();
+            if(calculadoraSeleccion.equals("1"))
+                calculadora = new CalculadoraBasica();
+            else if(calculadoraSeleccion.equals("2"))
+                calculadora = new CalculadoraCientifica();
+            else  System.out.println("Opción no válida");
+        System.out.println(calculadoraSeleccion);
+
         int numOpciones = (calculadora instanceof CalculadoraCientifica? 10 : 5);
         System.out.println(calculadora.imprimirMenu());
         boolean running = true;
@@ -14,20 +26,17 @@ public class Main {
             int opcion = 0;
             System.out.println("elige una operacion");
             try{
-                opcion = scanner.nextInt();
+                opcion = sc.nextInt();
                 if (opcion >=numOpciones) {throw new RuntimeException();}
                 System.out.println(calculadora.switchCalculadora(opcion));
                 running = false;
             } catch (InputMismatchException e) {
                 System.out.println("Error caracter");
-                scanner.next();
+                sc.next();
             } catch (RuntimeException e) {
                 System.out.println("Opcion no valida en menu");
             }
         }
-
-
-
 
     }
 }
